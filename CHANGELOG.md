13049852a245760aa2ddcc13dee5056c3d2f17f7
Ya Zahra <71648215+Ya-Zahra@users.noreply.github.com>
2023-04-03 04:17:24 +0330

    v0.0.8
    0.0.8
    Now ready for Firefox Nightly for Developers (Android) and Firefox Desktop

---
+++ CHANGELOG.md
@@ -0,0 +1,7 @@
+APK Downloader for Cafebazaar.ir
+
+0.0.8
+Now ready for Firefox Nightly for Developers (Android) and Firefox Desktop
+
+0.0.7
+Add support for Firefox Nightly for Developers (Android) - Thanks to  Muhammad Hussein Ammari - xmha97 (https://github.com/xmha97)
--- src/background.js
+++ src/background.js
@@ -1,5 +1,8 @@
+console.log('CAD:','background script loaded');
 browser.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) { 
+	console.log('CAD:','Listener loaded');
     if (tab.url && tab.url.startsWith('https://cafebazaar.ir/app/') && changeInfo.status == 'complete') {
+		console.log('CAD:','script injecting by background script');
         browser.tabs.executeScript(null, {
             file: '/src/injectScript.js',
             runAt: 'document_end'
